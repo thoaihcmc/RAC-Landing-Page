fa93ab66cd612a98b7ee63f274de5c073ad12686
vuthoaile <v.le@reply.de>
2020-07-27 22:44:16 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RAC-Landing-Page';
+  myimage1:string ="assets/images/image1.jpg";
+  myimage2:string ="assets/images/spin2.gif";
+  myimage3:string ="assets/images/spin2.gif";
+  isLoading =false;
+  isLoading2 =false;
+
+  toggleLoading = () => {
+    this.isLoading = true;
+    setTimeout(()=>{
+      this.isLoading = false;
+    }, 3000)
+  }
+
+  toggleLoading2 = () => {
+    this.isLoading2 = true;
+    setTimeout(()=>{
+      this.isLoading2 = false;
+    }, 3000)
+  }
 }
